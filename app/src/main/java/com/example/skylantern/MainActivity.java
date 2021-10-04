c48838642e5cf4f441613d313bab63df1b73cf8e
staryu <1072461014@gm.cute.edu.tw>
2021-10-04 23:12:11 +0800

    1-1

--- app/src/main/java/com/example/skylantern/MainActivity.java
+++ app/src/main/java/com/example/skylantern/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.skylantern;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
+    TextView txv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.btn);
+        txv = (TextView) findViewById(R.id.txv);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
     }
 }
